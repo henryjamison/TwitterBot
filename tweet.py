5edbf3b20f08d4995deaadb37befedf17d6a5b15
henryjamison <jamisonhc@appstate.edu>
2023-08-09 22:52:38 -0400

    Changed layout, using requests. No longer need selenium/chromium

--- tweet.py
+++ tweet.py
@@ -1,13 +1,11 @@
 import os
-import time
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
 import random
 import requests
 import tweepy
+from bs4 import BeautifulSoup
+import urllib.request
+
+
 
 # Authenticate with Twitter API
 CONSUMER_KEY = os.environ["CONSUMER_KEY"]
@@ -24,75 +22,105 @@ auth_v2 = tweepy.Client(BEARER_TOKEN, CONSUMER_KEY, CONSUMER_SECRET,ACCESS_TOKEN
 
 # client = tweepy.Client(consumer_key=CONSUMER_KEY,consumer_secret=CONSUMER_SECRET,access_token=ACCESS_TOKEN,access_token_secret=ACCESS_TOKEN_SECRET)
 
-def getImage():
-    # Start the web driver
-    try:
-        # chrome_service = webdriver.ChromeService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
-        options = webdriver.ChromeOptions()
-        options.add_argument('--no-sandbox')
-        options.add_argument('--headless')
-        options.add_argument('--disable-dev-shm-usage')
-        options.add_experimental_option("detach", True)
-        version = '114.0.5735.90'
-        # version = '112.0.5615.28'
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager(driver_version=version).install()), options=options)
-        # driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager(version=version).install()), options=options)
-        # driver = webdriver.ChromeService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+# def getImage():
+#     # Start the web driver
+#     try:
+#         # Load the webpage
+#         patent_number = random.randint(1000000, 9999999)
+#         url = f"https://patents.google.com/patent/US{patent_number}/en?oq={patent_number}"
+
+#         req = requests.get(url)
+#         soup = BeautifulSoup(req.text, features="lxml")
+#         meta = soup.find_all('meta',itemprop='full')
 
 
-        # Load the webpage
-        patent_number = random.randint(1000000, 9999999)
-        url = f"https://patents.google.com/patent/US{patent_number}/en?oq={patent_number}"
-        driver.get(url)
+#         ## TODO: Add a check to see if meta exists, if not just re run like normal, else grab up to 4 photos and download - named on index.
+#         ##      Update tweet method to grab all ids if media exists Might have to do chunked upload
+#         for img in meta:
+#             print(img['content'])
+    
+#         # response = requests.get(image_url)
 
-        time.sleep(0.5)
+#         body = title.text + "\nOwned By: " + owner.text + "\n" + "Patent Number: " + str(patent_number) + "\n" + url
+#         # Check if the request was successful
+#         if req.status_code == 200:
+#             # Save the image to a file
+#             filename = f"{patent_number}.jpg"
+#             with open(filename, "wb") as f:
+#                 f.write(req.content)
+#         sendTweet(body,filename)
+#     except NoSuchElementException:
+#         print("No picture found, searching again.")
+#         getImage()
 
-        titleXpath = '/html/body/search-app/search-result/search-ui/div/div/div/div/div/result-container/patent-result/div/div/div/h1'
-        title = driver.find_element(By.XPATH, titleXpath)
-        time.sleep(0.5)
-
-        ownerXpath = '//*[@id="wrapper"]/div[1]/div[2]/section/dl[1]/dd[2]'
-        owner = driver.find_element(By.XPATH, ownerXpath)
-        time.sleep(0.5)
-
-        divXpath = '//*[@id="figures"]/div/img[1]'
-        driver.find_element(By.XPATH, divXpath).click()
-        time.sleep(0.5)
-
-        img_Xpath = '//*[@id="image"]'
-        image_element = driver.find_element(By.XPATH, img_Xpath)
-
-        # Get the image URL
-        image_url = image_element.get_attribute("src")
-
-        # Download the image
-        response = requests.get(image_url)
-
-        body = title.text + "\nOwned By: " + owner.text + "\n" + "Patent Number: " + str(patent_number) + "\n" + url
-
-        # Check if the request was successful
-        if response.status_code == 200:
-            # Save the image to a file
-            filename = f"{patent_number}.jpg"
-            with open(filename, "wb") as f:
-                f.write(response.content)
-        sendTweet(body,filename)
-        driver.quit()
-    except NoSuchElementException:
-        driver.quit()
-        print("No picture found, searching again.")
-        getImage()
-
-def sendTweet(body,filename):
-    media = api.media_upload(filename=filename)
-    auth_v2.create_tweet(text=body, media_ids=[media.media_id])
-    # tweet_id = str(tweet_v2.data["id"])
-    # tweet_url = "https://twitter.com/PatentArtBot/status/"+tweet_id
-    # client.create_tweet(text=body, media_ids=media)
-    # api.update_status_with_media(body,filename)
-    os.remove(filename)
-    print("tweeted!")
+# def sendTweet(body,filename):
+#     media = api.media_upload(filename=filename)
+#     auth_v2.create_tweet(text=body, media_ids=[media.media_id])
+#     os.remove(filename)
+#     print("tweeted!")
     
 
-getImage()
+# getImage()
+def start():
+    patent_number = random.randint(1000000, 9999999)
+    url = f"https://patents.google.com/patent/US{patent_number}/en?oq={patent_number}"
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, features='lxml')
+    meta = soup.find_all('meta',itemprop='full')
+    ####### start()[0], runner()[1], etc..
+    return patent_number, url, req, soup, meta
+
+
+def get_body(soup, num, url):
+    # print(soup)
+    title = soup.title.text
+    parsed_title = title.split(' - ')[1].strip()
+    owner = soup.find('meta',scheme='assignee')['content']
+    if (owner == 'Individual'):
+        owner = soup.find('meta',scheme='inventor')['content']
+    body = parsed_title + "\nOwned By: " + owner + "\n" + "Patent Number: US" + str(num) + "\n" + url
+    return body
+    
+
+def trim_url_list(meta):
+    img_list = []
+    for img in meta:    
+        i = img['content']
+        img_list.append(i)
+    if img_list == []:
+        print("no pics found")
+        runner()
+    return img_list[:4]
+
+def download_imgs(img_list):
+    index = 0
+    file_list = []
+    for img in img_list:
+        filename = f"{index}.jpg"
+        urllib.request.urlretrieve(img, filename)
+        file_list.append(filename)
+        index+=1
+    return file_list
+
+def runner():
+    start_info = start()
+    patent_number = start_info[0]
+    url = start_info[1]
+    req = start_info[2]
+    soup = start_info[3]
+    meta = start_info[4]
+    i_list = trim_url_list(meta)
+    file_list = download_imgs(i_list)
+    body = get_body(soup,patent_number,url)
+    sendTweet(body,file_list)
+
+def sendTweet(body,file_list):
+    media_ids = [api.media_upload(i).media_id_string for i in file_list] 
+    auth_v2.create_tweet(text=body, media_ids=[media_ids])
+    for file in file_list:
+        os.remove(file)
+    print("tweeted!")
+
+
+runner()
 
